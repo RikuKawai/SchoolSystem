8cf005dcd1dd04f7bc52f69c7e7dc495f19db608
riku <riku@192.168.2.14>
2016-10-02 15:13:37 -0400

    student object class

---
+++ src/moe/thisis/SchoolSystem/Student.java
@@ -0,0 +1,108 @@
+package moe.thisis.SchoolSystem;
+
+public class Student {
+	private String firstName;
+	private String lastName;
+	private String streetAddress;
+	private String city;
+	private String province;
+	private String postalCode;
+	private String phoneNumber;
+	private String birthDate;
+	/**
+	 * @return the first name
+	 */
+	public String getFirstName() {
+		return firstName;
+	}
+	/**
+	 * @param firstName the first name to set
+	 */
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	/**
+	 * @return the last name
+	 */
+	public String getLastName() {
+		return lastName;
+	}
+	/**
+	 * @param lastName the last name to set
+	 */
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	/**
+	 * @return the street address
+	 */
+	public String getStreetAddress() {
+		return streetAddress;
+	}
+	/**
+	 * @param streetAddress the street address to set
+	 */
+	public void setStreetAddress(String streetAddress) {
+		this.streetAddress = streetAddress;
+	}
+	/**
+	 * @return the city
+	 */
+	public String getCity() {
+		return city;
+	}
+	/**
+	 * @param city the city to set
+	 */
+	public void setCity(String city) {
+		this.city = city;
+	}
+	/**
+	 * @return the province
+	 */
+	public String getProvince() {
+		return province;
+	}
+	/**
+	 * @param province the province to set
+	 */
+	public void setProvince(String province) {
+		this.province = province;
+	}
+	/**
+	 * @return the postal code
+	 */
+	public String getPostalCode() {
+		return postalCode;
+	}
+	/**
+	 * @param postalCode the postal code to set
+	 */
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
+	}
+	/**
+	 * @return the phone number
+	 */
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	/**
+	 * @param phoneNumber the phone number to set
+	 */
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	/**
+	 * @return the birth date
+	 */
+	public String getBirthDate() {
+		return birthDate;
+	}
+	/**
+	 * @param birthDate the birth date to set
+	 */
+	public void setBirthDate(String birthDate) {
+		this.birthDate = birthDate;
+	}
+}
