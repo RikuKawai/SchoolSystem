60f07c08ce1a3dcfacfa787b52762654b1c3d401
Quinlan <riku@thisis.moe>
2016-10-13 14:54:42 -0400

    not working

--- src/moe/thisis/SchoolSystem/StudentSystem.java
+++ src/moe/thisis/SchoolSystem/StudentSystem.java
@@ -5,6 +5,13 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+
+import moe.thisis.SchoolSystem.Student;
+import moe.thisis.SchoolSystem.StudentInput;
+
 public class StudentSystem {
 	/**
 	 * StudentSystem.java
@@ -13,7 +20,14 @@ public class StudentSystem {
 	 * @version 0.2.1
 	 * @date 10/02/2016
 	 */
-	static ArrayList<Student> studRecs = new ArrayList<Student>();
+	//static ArrayList<Student> studRecs = new ArrayList<Student>();
+	
+	private ArrayList<Student> studRecs;
+	
+	public ArrayList<Student> getStudRecs() {
+		return studRecs;
+	}
+	
 	static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 	static final String VERSION = "0.2";
 	
@@ -21,6 +35,8 @@ public class StudentSystem {
 				
 		System.out.println("StudentSystem v" + VERSION);
 		System.out.println("Quinlan McNellen, ICS4U, 2016\n");
+		
+		MenuController menu = new MenuController();
 		
 		boolean running = true;
 		
@@ -33,7 +49,7 @@ public class StudentSystem {
 				System.out.println("1: Add new record\n2: Print a record\n3: Print all records\n4: Remove a record\n5: null\n6: null\n7: null\n8: null\n9: null\n10: Quit\nh: Show this screen");
 				break;
 			case "1":
-				createRecord();
+				menu.entryAdd();
 				break;
 			case "2":
 				System.out.print("Enter record number: ");
@@ -62,49 +78,58 @@ public class StudentSystem {
 	 * @param i index of the record to print
 	 */
 	public static void printRecord(int i) {
-		try {
-			Student tempRecord = studRecs.get(i);
-			System.out.println("Printing information for student " + i);
-			System.out.println("First Name: " + tempRecord.getFirstName());
-			System.out.println("Last Name: " + tempRecord.getLastName());
-			System.out.println("Street Address: " + tempRecord.getStreetAddress());
-			System.out.println("City: " + tempRecord.getCity());
-			System.out.println("Province: " + tempRecord.getProvince());
-			System.out.println("Postal Code: " + tempRecord.getPostalCode());
-			System.out.println("Phone Number: " + tempRecord.getPhoneNumber());
-			System.out.println("Birth Date: " + tempRecord.getBirthDate());
-		} catch (IndexOutOfBoundsException e) {
-			System.out.println("Invalid student!");
-		}
+		//try {
+		//	Student tempRecord = studRecs.get(i);
+		//	System.out.println("Printing information for student " + i);
+		//	System.out.println("First Name: " + tempRecord.getFirstName());
+		//	System.out.println("Last Name: " + tempRecord.getLastName());
+		//	System.out.println("Street Address: " + tempRecord.getStreetAddress());
+		//	System.out.println("City: " + tempRecord.getCity());
+		//	System.out.println("Province: " + tempRecord.getProvince());
+		//	System.out.println("Postal Code: " + tempRecord.getPostalCode());
+		//	System.out.println("Phone Number: " + tempRecord.getPhoneNumber());
+		//	System.out.println("Birth Date: " + tempRecord.getBirthDate());
+		//} catch (IndexOutOfBoundsException e) {
+		//	System.out.println("Invalid student!");
+		//}
 	}
 	
 	/**
 	 * Print all student records
 	 */
 	public static void printAllRecords() {
-		for (int i = 0; i<studRecs.size(); i++) {
-			printRecord(i);
-		}
+		/**
+		 * for (int i = 0; i<studRecs.size(); i++) {
+		 * 	printRecord(i);
+		 * }
+		 */
 	}
 	
 	/**
 	 * Create and append a new student record
 	 * @throws IOException
 	 */
-	public static void createRecord() throws IOException {
-		studRecs.add(new Student());
-		int i = studRecs.size() - 1;
-		Student tempRecord = studRecs.get(i);
-		System.out.println("Creating Student " + i);
-		tempRecord.setFirstName(StudentInput.firstName());
-		tempRecord.setLastName(StudentInput.lastName());
-		tempRecord.setStreetAddress(StudentInput.streetAddress());
-		tempRecord.setCity(StudentInput.city());
-		tempRecord.setProvince(StudentInput.province());
-		tempRecord.setPostalCode(StudentInput.postalCode());
-		tempRecord.setPhoneNumber(StudentInput.phoneNumber());
-		tempRecord.setBirthDate(StudentInput.birthDate());
-		System.out.println("Student " + i + " Created!");
+	public void getStudentInput(Student student) throws IOException {
+		/**
+		 * studRecs.add(new Student());
+		 * int i = studRecs.size() - 1;
+		 * Student tempRecord = studRecs.get(i);
+		 * System.out.println("Creating Student " + i);
+		 * tempRecord.setFirstName(StudentInput.firstName());
+		 * tempRecord.setLastName(StudentInput.lastName());
+		 * tempRecord.setStreetAddress(StudentInput.streetAddress());
+		 * tempRecord.setCity(StudentInput.city());
+		 * tempRecord.setProvince(StudentInput.province());
+		 * tempRecord.setPostalCode(StudentInput.postalCode());
+		 * tempRecord.setPhoneNumber(StudentInput.phoneNumber());
+		 * tempRecord.setBirthDate(StudentInput.birthDate());
+		 * System.out.println("Student " + i + " Created!");
+		 */
+		
+		StudentInput input = new StudentInput();
+		
+		input.setStudent(student);
+		
 	}
 	
 	/**
@@ -113,27 +138,27 @@ public class StudentSystem {
 	 * @throws IOException
 	 */
 	public static void removeRecord() throws NumberFormatException, IOException {
-		System.out.print("Enter record number: ");
-		int i = Integer.parseInt(in.readLine());
-		System.out.print("Are you SURE you want to delete record " + i + "? (y/n): ");
-		switch (in.readLine().toLowerCase()) {
-		case "y":
-			try {
-				studRecs.remove(i);
-				System.out.println("Record " + i + " removed, records following it have been shifted down 1!");
-				break;
-			} catch (IndexOutOfBoundsException e) {
-				System.out.println("Invalid student! Aborting!");
-				break;
-			}
-		case "n":
-			System.out.println("Aborting!");
-			break;
-		default:
-			System.out.println("Invalid input, aborting!");
-			break;
-		}
-		return;
+		//System.out.print("Enter record number: ");
+		//int i = Integer.parseInt(in.readLine());
+		//System.out.print("Are you SURE you want to delete record " + i + "? (y/n): ");
+		//switch (in.readLine().toLowerCase()) {
+		//case "y":
+		//	try {
+		//		studRecs.remove(i);
+		//		System.out.println("Record " + i + " removed, records following it have been shifted down 1!");
+		//		break;
+		//	} catch (IndexOutOfBoundsException e) {
+		//		System.out.println("Invalid student! Aborting!");
+		//		break;
+		//	}
+		//case "n":
+		//	System.out.println("Aborting!");
+		//	break;
+		//default:
+		//	System.out.println("Invalid input, aborting!");
+		//	break;
+		//}
+		//return;
 	}
 
 }
